d171d3285eed185a62fded9bac9c0cea871c4c82
unknown <Administrador@JVN060104994.jv01.local>
2023-04-10 13:49:29 -0300

    remove cross

--- src/main/java/com/scraping/api/v1/controller/UserController.java
+++ src/main/java/com/scraping/api/v1/controller/UserController.java
@@ -1,21 +1,19 @@
 package com.scraping.api.v1.controller;
 
 import com.scraping.api.v1.dto.SignupRequestDTO;
-import com.scraping.api.v1.dto.UserDTO;
 import com.scraping.api.v1.mapper.Mapper;
 import com.scraping.domain.User;
 import com.scraping.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController("UserControllerV1")
 @RequestMapping(RestPath.BASE_PATH + "/user")
-@CrossOrigin("*")
 public class UserController {
 
     @Autowired
